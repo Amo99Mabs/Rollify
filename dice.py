f70d47708f0f2590e040d89556387a313a88ec46
Amo99Mabs <174100269+Amo99Mabs@users.noreply.github.com>
2025-09-10 22:17:55 +0200

    Improved Rollify by inputing validation and added automatic correction for sides
    - Included try/except to handle non-numeric inpust for both dice and sides
    - Added a maximum of 6 sides and user correction if an error occurs

--- dice.py
+++ dice.py
@@ -4,25 +4,34 @@ def dice_roll(dice,sides):
 
     roll = []
 
-    for i in range(0,dice):
+    for i in range(dice):
         face = random.randint(1,sides)
         roll.append(face)
 
     return roll
+try:
 
-dice = int(input("Dice: "))
+    dice = int(input("Dice: "))
+    if (dice <= 0):
+       print("Must have at least one dice!")
+       quit()
+except ValueError:
+    print("Invalid input! Please enter a number for dice.")
+    quit
 
-if (dice <= 0):
-    print("Must have at least one dice!")
-    quit()
+try:
 
-sides = int(input("Sides: "))
+    sides = int(input("Sides(max 6): "))
+    if (sides <= 0):
+       print("Must have at least one side!")
+       quit()
+    elif sides > 6:
+        print("Sides cannot be more than 6, setting sides = 6.")
+        sides = 6
+except ValueError:
+    print("Invalid input! Please enter a number of sides.")
+    quit
 
-if (sides <= 0):
-    print("Must have at least one side!")
-    quit()
 
 roll = dice_roll(dice, sides)
-
-
-print(roll)
+print("You rolled:", roll)
